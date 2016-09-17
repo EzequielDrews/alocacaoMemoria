8ea841b856d3d876291c45041fad04bac13fa031
ezequieldrews <ezequiel.losekann@hotmail.com>
2016-09-16 21:50:14 -0300

    subindo codigo calloc

--- alocacaoDinamica.c
+++ alocacaoDinamica.c
@@ -6,7 +6,7 @@ main()
 	char *memoria_alocada;
 	//aloca a quantidade de memoria solicitada (50 x char);
 	//e inicializa o ponteiro com a posicao da memoria
-	memoria_alocada = malloc (50 * sizeof(char));
+	memoria_alocada = calloc (50, sizeof(char));
 	//se o ponteiro foi inicializado com o valor nulo
 	if(memoria_alocada == NULL)
 	{
